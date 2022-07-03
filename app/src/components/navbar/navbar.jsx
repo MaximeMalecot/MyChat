46ce50f3d8544b7af40610c4716ca0d7c25407c4
jsaleix <julian.saleix@gmail.com>
2022-07-03 12:48:56 +0200

    navbar integration

--- app/src/components/navbar/navbar.jsx
+++ app/src/components/navbar/navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import classes from './navbar.module.scss';
+import SearchIcon from '../../assets/svg/search-icon.svg';
+import MessengerIcon from '../../assets/svg/messenger-icon.svg';
 
 export default function Navbar(){
     return(
-        <div>
+        <div className={classes.navbar}>
             <img src={""} alt="Logo"/>
+            <div className={classes.searchDiv}>
+                <img src={SearchIcon} alt=""/>
+                <input type="test" placeholder='Search on GESbook'/>
+            </div>
+            <div className={classes.tabs}>
+                <div className={`${classes.tabItem} ${classes.messengerIcon}`}>
+                    <img src={MessengerIcon} alt="messenger icon"/>
+                </div>
+                <div className={`${classes.tabItem} ${classes.profileIcon}`}>
+                    <img src={"https://i.stack.imgur.com/l60Hf.png"} alt="profile icon"/>
+                </div>
+            </div>
         </div>
     )
 }
