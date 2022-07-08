fda8406401ed349b68edf180522f084c3a7c481a
francisco.leblanc.sacha <francisco.leblanc.sacha@gmail.com>
2022-07-08 19:11:51 +0200

    Formulaire field v0.1

--- app/src/pages/admin/createField.jsx
+++ app/src/pages/admin/createField.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import styles from './filiere.module.scss';
+import {API} from '../../constants/base';
 
 export default function Filiere(){
     const [name, setName] = useState('cegenredefiliere');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
     return(
         <div className={styles.container}>
             <h1>Creation de filière</h1>
-            <form className={styles.form}>
+            <form onSubmit={handleSubmit} className={styles.form}>
                 <label htmlFor="name"> Name :</label>
-                <input id='name' type="text"/>
+                <input id='name' onChange={(e) => {setName(e.target.value)}} type="text"/>
                 <button type='submit'>Créer</button>
             </form>
         </div>
