c33773666ad7d5ceb91be254bbef4a2568c886d1
Clarence Potel <potel.clarence@gmail.com>
2022-07-20 00:25:37 +0200

    debut form

--- app/src/pages/profile/index.jsx
+++ app/src/pages/profile/index.jsx
@@ -5,42 +5,44 @@ export default function Profile(){
     const [email, setEmail] = useState('cegenredemail.com');
     const [name, setName] = useState('Jean Boris');
     const [password, setPassword] = useState('en attente');
+    const [showForm, setShowForm] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email, name, password);
+    }
     return(
         <div className={styles.container}>
-            {/* <h1>Mon profil</h1>
-            <section className={styles.myProfil}>
-                <img className={styles.avatar} width="100px" height="100px" src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Wayfarers&hairColor=Blonde&facialHairType=BeardMajestic&facialHairColor=Black&clotheType=ShirtCrewNeck&clotheColor=Blue03&eyeType=Squint&eyebrowType=DefaultNatural&mouthType=Disbelief&skinColor=Pale" alt="Photo de profil"  />
-                <h2>Nom Prenom</h2>
-            </section>
-            <h2>Mes informations</h2>
-            <form className={styles.form}>
-                <label htmlFor="email">Email :</label>
-                <input id='email' type="email" value={email}/>
-                <label htmlFor="password">Mot de passe :</label>
-                <input type="password" value={password} />
-                <button type='submit'>Edit</button>
-            </form>
-            <p>Classe : IW1</p>
-            <form className={styles.form} action="">
-                <div>
-                    <input type="checkbox" name="css" id="css" />
-                    <label htmlFor="css" style={{marginLeft: '10px'}}>Css</label>
-                </div>
-                <div>
-                    <input type="checkbox" name="React" id="React" />
-                    <label htmlFor="React" style={{marginLeft: '10px'}}>React</label>
-                </div>
-                <div>
-                    <input type="checkbox" name="Breakdance.js" id="Breakdance.js" />
-                    <label htmlFor="Breakdance.js" style={{marginLeft: '10px'}}>Breakdance.js</label>
-                </div>
-                <div>
-                    <input type="checkbox" name="Html" id="Html" />
-                    <label htmlFor="Html" style={{marginLeft: '10px'}}>Html</label>
-                </div>
-            </form> */}
-            <div className={styles.content}>
+            {showForm ?
+                <form className={styles.cardForm}>
+                    <p className={styles.close} onClick={()=>setShowForm(false)}>x</p>
+                    <label htmlFor="email">Email :</label>
+                    <input id='email' type="email" value={email}/>
+                    <label htmlFor="password">Mot de passe :</label>
+                    <input type="password" value={password} />
+                    <p>Classes : IW</p>
+                    <div>
+                        <input type="checkbox" name="css" id="css" />
+                        <label htmlFor="css" style={{marginLeft: '10px'}}>Css</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="React" id="React" />
+                        <label htmlFor="React" style={{marginLeft: '10px'}}>React</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="Breakdance.js" id="Breakdance.js" />
+                        <label htmlFor="Breakdance.js" style={{marginLeft: '10px'}}>Breakdance.js</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="Html" id="Html" />
+                        <label htmlFor="Html" style={{marginLeft: '10px'}}>Html</label>
+                    </div>
+                    <button type='submit' onSubmit={handleSubmit}>Edit</button>
+                </form>
+                :
+                <div className={styles.content}>
                 <div className={styles.card}>
+                    <p className={styles.edit} onClick={()=>setShowForm(true)}>edit</p>
                     <div className={styles.firstinfo}>
                         <img width='120px' height="120px" src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Wayfarers&hairColor=Blonde&facialHairType=BeardMajestic&facialHairColor=Black&clotheType=ShirtCrewNeck&clotheColor=Blue03&eyeType=Squint&eyebrowType=DefaultNatural&mouthType=Disbelief&skinColor=Pale" alt="Photo de profil"  />
                         <div className={styles.profileinfo}>
@@ -49,6 +51,7 @@ export default function Profile(){
                             <p className={styles.bio}>Je suis en classe de techo IW1</p>
                         </div>
                     </div>
+
                 </div>
                 <div className={styles.badgescard}>
                     <span className="devicon-docker-plain-wordmark colored"></span>
@@ -56,7 +59,8 @@ export default function Profile(){
                     <span className="devicon-mongodb-plain-wordmark colored"></span>
                     <span className="devicon-react-original-wordmark colored"></span>
                 </div>
-            </div>
+            </div>    
+            }
         </div>
     )
 }
